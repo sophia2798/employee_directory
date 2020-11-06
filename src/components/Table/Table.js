c9c8924eb0bbef1a019c33e1ef69a3582f5e9588
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-11-05 20:11:40 -0800

    sort by dob

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -34,21 +34,35 @@ class RowContainer extends Component {
             this.setState({ employees: nameSortArr });
         }
     };
+
+    handleSortByDate = () => {
+        this.setState({ nameSort: true });
+        if (this.state.nameSort) {
+            const dateSortArr = this.state.employees.sort((a,b) => {
+                return a.dob.date < b.dob.date? -1 : a.dob.date > b.dob.date ? 1 : 0;
+            })
+            this.setState({ employees: dateSortArr });
+        }
+    };
     
     render() {
         return (
-            <div>
+            <div className="table-container">
+            <div className="dropdown">
+                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Sort By
+                </button>
+                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a className="dropdown-item" onClick={this.handleSortByFirstName} href="#">First Name</a>
+                    <a className="dropdown-item" onClick={this.handleSortByLastName} href="#">Last Name</a>
+                    <a className="dropdown-item" onClick={this.handleSortByDate} href="#">D.O.B.</a>
+                </div>
+            </div>
             <table>
                 <thead>
                 <tr>
                     <th>Image</th>
-                    <th className="dropdown-toggle" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Name
-                    </th>
-                    <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                        <a className="dropdown-item" onClick={this.handleSortByFirstName}>Sort by First</a>
-                        <a className="dropdown-item" onClick={this.handleSortByLastName}>Sort by Last</a>
-                    </div>
+                    <th>Name</th>
                     <th>Phone</th>
                     <th>Email</th>
                     <th>DOB</th>
@@ -57,7 +71,7 @@ class RowContainer extends Component {
                 <tbody>
                 {this.state.employees.map(employee => {
                     return (
-                        <Row id={employee.id.value} name={`${employee.name.first} ${employee.name.last}`} email={employee.email} phone={employee.phone} image={employee.picture.thumbnail} dob={moment(employee.dob.date).format('MMM Do YYYY')}/>
+                        <Row id={employee.id.value} name={`${employee.name.first} ${employee.name.last}`} email={employee.email} phone={employee.phone} image={employee.picture.thumbnail} dob={moment(employee.dob.date).format('L')}/>
                     )
                 })}
                 </tbody>
